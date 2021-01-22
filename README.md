95b64baebae937b02a8bb21376a9e807a5d77833
Sisay Chala <chala@fit.fraunhofer.de>
2021-01-22 23:09:15 +0100

    code refactoring

--- README.md
+++ README.md
@@ -4,3 +4,9 @@ The purpose of his application is to automatically generate pert chart from task
 
 ## Requirement
 `pip install graphviz`
+
+# Credits and References
+- btree.py - http://www.graphviz.org/pdf/dotguide.pdf Figure 13
+- https://stackoverflow.com/questions/35064304/runtimeerror-make-sure-the-graphviz-executables-are-on-your-systems-path-aft
+- https://www.programcreek.com/python/example/104476/graphviz.Digraph
+- https://graphviz.readthedocs.io/en/stable/examples.html#structs-py
--- pertchart.py
+++ pertchart.py
@@ -8,6 +8,8 @@
 
 from graphviz import Digraph, nohtml
 import ast
+import logging
+import sys
 
 # Data
 # Provide tasks as list of tuples like:("Task id", "start", "duration", "end", "responsible", "predecessor)
@@ -16,66 +18,69 @@ import ast
 # For the final tasks preced "END", 
 #   e.g., ("END", "start", "duration", "end", "responsible", "Task id4"), ("END", "start", "duration", "end", "responsible", "Task id5")
 
-filename = "datafile.txt"
+def create_pert_chart(filename):
+    task_list = []
+    try:
+        with open(filename) as f:
+            for line in f:
+                values = ast.literal_eval(line)
+                task_list.append(values)
+    except:
+        logging.warning("Cannot generate PERT chart. File does not exist -> " + filename)
+        sys.exit(1)
+            
+    a = task_list
 
-task_list = []
-with open(filename) as f:
-    for line in f:
-        values = ast.literal_eval(line)
-        task_list.append(values)
-        
-a = task_list
+    # Graph Instance
+    g = Digraph('g', 
+                filename='PERT.gv',
+                node_attr={'shape': 'Mrecord', 
+                           'height': '.1'})
 
-# Graph Instance
-g = Digraph('g', 
-            filename='PERT.gv',
-            node_attr={'shape': 'Mrecord', 
-                       'height': '.1'})
+    # configurations
+    fill_color = 'grey93'
 
-# configurations
-fill_color = 'grey93'
+    g.attr(rankdir='LR')
+    g.attr('node', shape='record')
 
-g.attr(rankdir='LR')
-g.attr('node', shape='record')
+    # Nodes
 
-# Nodes
+    for i in range(len(a)):
+        if a[i][0] == "END":
+                continue
+        g.node(a[i][0], 
+               nohtml('<f0>' + 
+                      a[i][0] + 
+                      ' |{' + a[i][1] + '|' + a[i][2] + '|' + a[i][3] + '}|<f2>' + 
+                      a[i][4]), 
+               fillcolor=fill_color, 
+               style='filled',
+               color='red'
+              )
 
-for i in range(len(a)):
-    if a[i][0] == "END":
-            continue
-    g.node(a[i][0], 
-           nohtml('<f0>' + 
-                  a[i][0] + 
-                  ' |{' + a[i][1] + '|' + a[i][2] + '|' + a[i][3] + '}|<f2>' + 
-                  a[i][4]), 
-           fillcolor=fill_color, 
-           style='filled',
-           color='red'
-          )
+    # Edges
+    '''
+    g.edge('node0:f2', 'node4:f1') # connect edges with connetion points <f2> and <f1>
+    g.edge('node0', 'node1')
+    '''
 
-# Edges
-'''
-g.edge('node0:f2', 'node4:f1') # connect edges with connetion points <f2> and <f1>
-g.edge('node0', 'node1')
-'''
+    try:
+        for i in a:
+            #g.edge(i[3] + ':f2', i[0] + ':f0')
+            if i[0] == "END":
+                g.edge(i[5], "FINISH")
+            else:
+                g.edge(i[5], i[0])
+    except:
+        logging.info("Unexpected error. Check your inputs")
+    finally:
+        logging.info("PERT chart generated successfully")
 
-try:
-    for i in a:
-        #g.edge(i[3] + ':f2', i[0] + ':f0')
-        if i[0] == "END":
-            g.edge(i[5], "FINISH")
-        else:
-            g.edge(i[5], i[0])
-except:
-    print("Unexpected error. Check your inputs")
-finally:
-    print("PERT chart generated successfully")
+    g.view()
 
-g.view()
-
-# Credits and References
-# btree.py - http://www.graphviz.org/pdf/dotguide.pdf Figure 13
-''' https://stackoverflow.com/questions/35064304/runtimeerror-make-sure-the-graphviz-executables-are-on-your-systems-path-aft
-https://www.programcreek.com/python/example/104476/graphviz.Digraph
-https://graphviz.readthedocs.io/en/stable/examples.html#structs-py
-'''
+if __name__ == '__main__':
+    if len(sys.argv) >=2:
+        filename = sys.argv[1] #"datafile.txt"
+        create_pert_chart(filename)
+    else:
+        logging.info("Usage: pertchart <filename>")
