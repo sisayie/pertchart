70d155e5dd90e9fc863526d1c78e409f7338d5cd
Sisay A. Chala <sisayie@gmail.com>
2022-10-26 12:36:54 +0200

    Update conftest.py

--- conftest.py
+++ conftest.py
@@ -2,6 +2,8 @@ import pytest
 
 @pytest.fixture()
 def setup():
-    from pertchart import PertChart
-    pc = PertChart()
-    return pc
+        from pertchart import PertChart
+        pc = PertChart()
+        tasks = pc.getInput('sample_test_cases.json')
+        pc.create_pert_chart(pc.calculate_values(tasks))
+        return "Sucess"
