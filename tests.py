e3cc1b28b84ebed6ad20147b49ee9406747a10a1
Sisay A. Chala <sisayie@gmail.com>
2022-10-26 12:12:21 +0200

    Update tests.py

--- tests.py
+++ tests.py
@@ -2,8 +2,8 @@ import pytest
 
 @pytest.mark.usefixtures("setup")
 class TestAbc:
-    def test_class(self):
+    def test_class1(self):
         print("test class 1")
 
-    def test_demo1(self):
+    def test_class2(self):
         print("test class 2")
