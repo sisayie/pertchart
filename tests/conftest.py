14acc819e5db4927acb94a4c83e58e8629ca99a5
Sisay Chala <chala@fit.fraunhofer.de>
2022-11-02 10:20:07 +0100

    Organizing tests

--- tests/conftest.py
+++ tests/conftest.py
@@ -2,7 +2,7 @@ import pytest
 
 @pytest.fixture()
 def setup():
-        from pertchart import PertChart
+        from pertchart.pertchart import PertChart
         pc = PertChart()
         tasks = pc.getInput('tests/input/sample_test_cases.json')
         pc.create_pert_chart(pc.calculate_values(tasks))
